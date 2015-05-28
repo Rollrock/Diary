bea9ba6ee89f858ca52445ef1cee6b5595d98dfd
zhuang chaoxiao <zhuangchaoxiao@163.com>
2015-05-28 18:24:53 +0800

    初始化

---
+++ Diary/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Diary
+//
+//  Created by zhuang chaoxiao on 15-5-28.
+//  Copyright (c) 2015年 zhuang chaoxiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Diary/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Diary
+//
+//  Created by zhuang chaoxiao on 15-5-28.
+//  Copyright (c) 2015年 zhuang chaoxiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
